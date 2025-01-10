75c2cd4b53b3605d1dcb338c167b6bd78fc76e5c
TomasVaquerin <vaquerinjimenez@gmail.com>
2025-01-10 15:51:19 +0100

    first commit

---
+++ Personas/Models/Persona.cs
@@ -0,0 +1,8 @@
+﻿namespace Personas.Models;
+
+public class Persona
+{
+    public string Nombre { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+}
